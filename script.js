57ed5a503b513cf08e4769567d1ce70f6fd963fa
ericdu6865 <zhengyuandu2005311@gmail.com>
2024-06-27 13:36:35 -0400

    updated script.js

--- script.js
+++ script.js
@@ -30,3 +30,41 @@ function greetUser(Name, time)
 }
 
 console.log(`Exercise 5 result:`, greetUser("Eric", "Morning"))
+
+function maxofThree(a,b,c) {
+    if(a>=b && a>=c ){
+        return a;
+    }
+    else if (b>=a && b>=c) {
+        return b;
+    } 
+    else {
+        return c;
+    }
+}
+console.log(`Exercise 6 Result:`, maxofThree(5, 10, 8));
+
+function calculateTip(totalAmount, tipPercentage )
+{
+    return (totalAmount * tipPercentage) / 100;
+
+}
+console.log('Exercise 7 Result:', calculateTip(50, 20));
+
+
+function convertTemperature(temperature, scale)
+{
+    if(scale === `C`)
+        {
+            return(temperature * 9/5)+32;
+        }
+        else if(scale === `F`)
+            {
+                return(temperature-32)*5/9;
+            }
+                else {
+                    return `Invalid scale`;
+                }
+            }
+        
+        console.log('Exercise 8 Result:', convertTemperature(32, "C"));
